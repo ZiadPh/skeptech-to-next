aaa25b15f5db467c63f5f1d5abf02d829a75ef17
Ziad Youssef <62078458+ZiadPh@users.noreply.github.com>
2024-03-18 00:18:00 +0200

    DevMo Page Added
    Finished DevMo website and updated its page into the skeptech website

--- pages/devMo.js
+++ pages/devMo.js
@@ -1,13 +1,15 @@
 import Nav from "../components/nav"
 import Image from "next/image"
 import DM3D from '../assets/devMo.png'
-import DMgif from '../assets/DMgif.gif'
-import Cref1 from '../assets/ref1.png'
-import Cref2 from '../assets/ref2.png'
-import Cref3 from '../assets/ref3.png'
-import Font1 from '../assets/font1.png'
-import Font2 from '../assets/font2.gif'
-import Color from '../assets/p11-color.png'
+import DPLT from '../assets/ssd.png'
+import NPLT from '../assets/ssn.png'
+import D3D from '../assets/day3d.png'
+import N3D from '../assets/night3d.png'
+import room from '../assets/realRoom.jpg'
+import s3d from '../assets/setup3d.png'
+import arr from '../assets/aurora.png'
+import MI from '../assets/MI.png'
+import rlr from '../assets/ruler.png'
 import { useState , useEffect } from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
@@ -86,16 +88,22 @@ return(
                     <div>
                     {/* Container for first Paragraph*/}
                         <div>
-                            Background:
+                            Summary:
                         </div>
                         <div>
-                            Pixel10 Studio is a startup agency specializing in providing high-quality Architectural Visualisation, Animation, Virtual Reality, and Interior Design services. With a team of talented professionals, they have successfully executed various projects for clients in the real estate, construction, and interior design sectors.
-                        </div>
+                        The project involves the creation of an interactive and engaging online curriculum vitae (CV) that showcases a developer&apos;s workspace using 3D visualization. This virtual experience will enable visitors to explore the developer&apos;s environment, providing an immersive insight into their work area, including details of the computer setup and any relevant gadgets or tools.</div>
                     </div>
                 </div>
             </div>
             <div className="Section-2">
-                <Image className="gif" src={DMgif} width={viewportSize.width}  alt="PixelGif"/>
+                <iframe
+                frameBorder={0}
+                allowFullScreen=""
+                scrolling="no"
+                allow="autoplay;fullscreen"
+                src="https://onelineplayer.com/player.html?autoplay=true&autopause=false&muted=true&loop=true&url=https%3A%2F%2Fwww.dropbox.com%2Fscl%2Ffi%2Fjoq46111m1erzd317d1ly%2FIntro.mp4%3Frlkey%3Dh8exxw6p3sb8td0mss20t04t3%26raw%3D1&poster=&time=false&progressBar=false&overlay=true&muteButton=false&fullscreenButton=false&style=light&quality=720p&playButton=false"
+                style={{ height: "100%", width: "100%", aspectRatio: "1598 / 782" }}
+                />
             </div>
             <div className="section-3">
                 <div>
@@ -111,7 +119,7 @@ return(
                             <span>
                             Highlight Services:&nbsp;
                             </span>
-                            Clearly present their core services, providing detailed information about each service offering, the process involved, and how they add value to clients' projects.                    </li> 
+                            Clearly present their core services, providing detailed information about each service offering, the process involved, and how they add value to clients&apos; projects.                    </li> 
                         <li> {/* #3 */}
                             <span>
                             Enhance User Experience:&nbsp;
@@ -126,7 +134,7 @@ return(
                             <span>
                             Improve Brand Image:&nbsp;
                             </span>
-                            Reflect the professionalism, creativity, and innovation that Pixel10 Studio brings to each project through the website's design and content.                    </li>
+                            Reflect the professionalism, creativity, and innovation that Pixel10 Studio brings to each project through the website&apos;s design and content.                    </li>
                         <li> {/* #6 */}
                             <span>
                             SEO Optimization:&nbsp;
@@ -136,45 +144,63 @@ return(
                 </div>
 
             </div>
-            <div className="section-4 ">
-                <h1>Client References:</h1>
-                <div className="Carousel">
-                    <Carousel dynamicHeight={true} width={"68%"} >
-                        <div>
-                            <Image src={Cref1} alt="null"/>
-                            <p className="legend">Eleven V visualisation studio</p>
-                        </div>
-                        <div>
-                            <Image src={Cref2} alt="null" />
-                            <p className="legend">Smiling Wolf creative agency</p>
-                        </div>
-                        <div>
-                            <Image src={Cref3} alt="null"/>
-                            <p className="legend">Wow Tapes creative agency</p>
-                        </div>
-                    </Carousel>
-                </div>
-
-            </div>
             <div className="section-5">
                 <div className="s5-wrapper">
-                    <h1>Design Aspects</h1>
+                    <h1>The Process</h1>
                     
-                    <h2>1. Typography and Fonts</h2>
+                    <h2>1. 3D Model</h2>
                     <div className="fonts">
-                        <Image src={Font1} alt="Null"/>
-                        <Image src={Font2} alt="Null"/>
+                        <Image src={room} alt="Null"/>
+                        <Image src={s3d} alt="Null"/>
                     </div>
+                    <p>The main idea started with a trial to replicate the computer setup owned by the client on Blender, as it worked pretty well, we had a further interview with our client trying to identify what his visions of a perfect room setup would look like, not to mention that we also paid a visit to his room to gather a better to be able to work towards our overall 3d model. <br/><br/> We also looked Into exaggerating the practical lighting to give it more of the gamer vibes through the RGB elements and screen lights as well as the Hexagonal Light panels that were inspired by the Govee light products.</p>
+                    <h2>2. Background ( Aurora UI )</h2>
+                    <Image src={arr}/>
+                    <p>The Background Design was chosen to take the style of the aurora UI so it can act as a realm for the existent 3D model to add some depth, give off a dreamy look and blend seamlessly into the overall look.</p>
+                    <h2>3. Colours</h2>
+                    <div>
+                        <div className="grid">                        
+                            <Image src={D3D} alt="Null"/>
+                            <Image src={DPLT} alt="Null"/>
+                        </div>
+                        <div className="grid">                        
+                            <Image src={N3D} alt="Null"/>
+                            <Image src={NPLT} alt="Null"/>
+                        </div>
+                    </div>
+                    <p>Most of the colours were mainly dependent on the colours of the practical lighting in both day and night themes in the 3D model and were made to serve the same dreamy look.<br/>
+For  the Text and UI elements the choice of Beige and Black was mainly to serve the text readability throughout an interchanging Background.</p>
 
-                    <h2>2. Color</h2>
-                    <Image src={Color}/>
-
-                    <h2>3. Layout and Prototype</h2>
-                    {/* Figma Embed */}
-                    <iframe className="embed" src="https://www.figma.com/embed?embed_host=share&amp;url=https%3A%2F%2Fwww.figma.com%2Fproto%2FrvQX9LdcjoJEjDmwcguS2N%2FPixel-10%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D5-254%26viewport%3D137%252C449%252C0.11%26t%3DLLV5Di45F6bm9d6C-1%26scaling%3Dscale-down%26starting-point-node-id%3D5%253A254%26show-proto-sidebar%3D1%26mode%3Ddesign" allowfullscreen sandbox="allow-same-origin allow-scripts allow-pointer-lock allow-forms"></iframe>
-
-                    <h2>4. Animations and Transitions</h2>
-                    <div className="animation-grid"></div>
+                    <h2>4. Fonts and UI Elements</h2>
+                    <div className="fonts">                        
+                            <Image src={MI} alt="Null"/>
+                            <Image src={rlr} alt="Null"/>
+                    </div>
+                    <p>The choice of the font family Made Infinity was to give off both the digital and playful vibes which suits our clients lifestyle that involves Video gaming and Software Development.
+                        <br/>
+                        <br/>
+                        On the other hand, the Ruler on the margins of the page was more of a Functionality decision to serve as a timeline of his career that is demonstrated into his CV.
+                         </p>
+                    <h2>5. Animations</h2>
+                    <p>Animations were a key factor in the whole experience where different animations throughout the experience were used to deliver different feelings to the user:</p>
+                    <iframe
+                    frameBorder={0}
+                    allowFullScreen=""
+                    scrolling="no"
+                    allow="autoplay;fullscreen"
+                    src="https://onelineplayer.com/player.html?autoplay=true&autopause=false&muted=true&loop=true&url=https%3A%2F%2Fwww.dropbox.com%2Fscl%2Ffi%2Fhkfudahiziwsrtrhr5ocp%2FFade.mp4%3Frlkey%3Du7acrpsy2vzrzzvhum6s8seo0%26raw%3D1&poster=&time=false&progressBar=false&overlay=true&muteButton=false&fullscreenButton=false&style=light&quality=720p&playButton=false"
+                    style={{ height: "100%", width: "100%", aspectRatio: "1598 / 782" }}
+                    />
+                    <p>Fading Animations: Most of the changes that happens between the day and night theme occurs through a fading animation to give off the feeling of the passage of time between day and night as well as to serve into the overall dreamy vibe.</p>
+                    <iframe
+                    frameBorder={0}
+                    allowFullScreen=""
+                    scrolling="no"
+                    allow="autoplay;fullscreen"
+                    src="https://onelineplayer.com/player.html?autoplay=true&autopause=false&muted=true&loop=true&url=https%3A%2F%2Fwww.dropbox.com%2Fscl%2Ffi%2Ffkle54g99v3g7mhzwq1j7%2FReveal.mp4%3Frlkey%3Dpx2kqgi9yw2d6r0jrbozs9eed%26raw%3D1&poster=&time=false&progressBar=false&overlay=true&muteButton=false&fullscreenButton=false&style=light&quality=720p&playButton=false"
+                    style={{ height: "100%", width: "100%", aspectRatio: "1352 / 780" }}
+                    />
+                    <p>Text Reveal Animation: This was mainly intended to give off more of this digital look takes up a massive part of the client&apos;s lifestyle, not to mention how it synchronises with the change of the elements on the screens in the 3D model.</p>
                 </div>
             </div>
             <Footer/>
